c754727e439deb838952702f98c781c0e1aa6773
Kari Vigna <karivigna24@gmail.com>
2023-07-02 07:32:01 -0700

    README added and updated

---
+++ README.md
@@ -0,0 +1,33 @@
+# _Mr. Roboger_
+
+#### By _Kari Vigna_
+
+#### _This application takes a number then transforms it into a secret message._
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Javascript_
+* _brain juice_
+
+## Description
+
+_{This application takes an integer and returns a sequence of elements from 0 to inputted integer. All elements containing a 1 will be replaced with "Beep!", elements containing a 2 will be replaced with "Boop!" and elements containing a 3 will be replaced with "Won't you be my neighbor?"}_
+
+## Setup/Installation Requirements
+
+* _Clone this repository to desktop._
+* _Navigate this projects top level directory._
+* _Open roboger/index.html in your browser._
+
+## Known Bugs
+
+* _Does not return "0" with sequence of numbers._
+* _Only replaces first instance of 1._
+
+## License
+
+_MIT_
+
+Copyright (c) _07/02/2023_ _Kari Vigna_
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
 window.onload = function() {
-    console.log("page loaded!");
     let form = document.querySelector("form");
     form.onsubmit = function(event) {
         event.preventDefault();
-        console.log("form submitted!");
     
         let inputNum = document.getElementById("num-input").value;
-        // let theResult = document.querySelector("span#result");
         const unfold = (accumulator, length) => length <= 0 ? accumulator : unfold([length, ...accumulator], length -1);
-        let outputArray = unfold([], inputNum).join().split(",");
+        let outputArray = unfold([], inputNum).join().split(", ");
         
-        return outputArray.forEach(function(number) {
-            const resultDisplay = document.createElement("p");
-            resultDisplay.append(number);
-            document.body.append(resultDisplay);
-            console.log(number);
+        
+        for (let i = 0; i < outputArray.length; i +=1) {
+            if (inputNum.includes("1") === true)  {
+                let contain1 = outputArray.toString().replace("1", "Beep!");
+                let resultDisplay = document.createElement("p");
+                document.body.append(contain1)};
+
+            } 
+            // else if (inputNum.includes("2") === true) {
+            //     const resultDisplay = document.createElement("p");
+            //     const contain2 = outputArray.toString().replace("2", "Boop!");
+            //     return console.log(contain2) 
+            //     document.body.append(contain2);
             
-        });
         
-        }; 
+        
+        
+        // return outputArray.forEach(function(number) {
+        //     if (number.includes("1") === true) {
+        //         const resultDisplay = document.createElement("p");
+        //         const contain1 = outputArray.toString().replace("1", "Beep!");
+        //         return document.body.append(contain1);
+        //     }
+        //     else if (number.includes("2") === true) {
+        //         const resultDisplay = document.createElement("p");
+        //         const contain2 = outputArray.toString().replace("2", "Boop!")
+        //         return document.body.append(contain2);
+        //     }
+        //     else {
+        //     return document.body.append(number);}
+        // });
+        
+        };
     };
