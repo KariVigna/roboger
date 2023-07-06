6071ddfea16d892444a720dde596c187f8a76538
Kari Vigna <karivigna24@gmail.com>
2023-07-05 22:14:28 -0700

    hierarchy for conditionals updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,40 @@
+
+// UI Logic
 window.onload = function() {
     let form = document.querySelector("form");
     form.onsubmit = function(event) {
-        event.preventDefault();
-    
+        
         let inputNum = document.getElementById("num-input").value;
-        const unfold = (accumulator, length) => length <= 0 ? accumulator : unfold([length, ...accumulator], length -1);
-        let outputArray = unfold([], inputNum).join().split(", ");
+        const result = beepBoop(inputNum);
+        document.createElement("p") = pElement;
+        pElement.innerText = result;
         
-        
-        for (let i = 0; i < outputArray.length; i +=1) {
-            if (inputNum.includes("1") === true)  {
-                let contain1 = outputArray.toString().replace("1", "Beep!");
-                let resultDisplay = document.createElement("p");
-                document.body.append(contain1)};
-
-            } 
-            // else if (inputNum.includes("2") === true) {
-            //     const resultDisplay = document.createElement("p");
-            //     const contain2 = outputArray.toString().replace("2", "Boop!");
-            //     return console.log(contain2) 
-            //     document.body.append(contain2);
-            
-        
-        
-        
-        // return outputArray.forEach(function(number) {
-        //     if (number.includes("1") === true) {
-        //         const resultDisplay = document.createElement("p");
-        //         const contain1 = outputArray.toString().replace("1", "Beep!");
-        //         return document.body.append(contain1);
-        //     }
-        //     else if (number.includes("2") === true) {
-        //         const resultDisplay = document.createElement("p");
-        //         const contain2 = outputArray.toString().replace("2", "Boop!")
-        //         return document.body.append(contain2);
-        //     }
-        //     else {
-        //     return document.body.append(number);}
-        // });
-        
-        };
+        event.preventDefault();
     };
+};
+
+// Business Logic
+
+function beepBoop(num){
+    let numbersArray = [];
+    for(let i = 0; i <= num; i += 1){
+        numbersArray.push(i);
+    }
+
+    let outputArray = [];
+
+    numbersArray.forEach(function(element){ 
+        element = element.toString()
+        console.log(element.includes("3") === true);
+        if (element.includes("3") === true) {
+            outputArray.push("Won't you be my neighbor?");
+        } else if (element.includes("2") === true) {
+            outputArray.push("Boop")
+        } else if (element.includes("1") === true) {
+            outputArray.push("Beep");
+        } else {
+            outputArray.push(element);
+        }
+    })
+    return outputArray;
+}
--- tests.md
+++ tests.md
@@ -1,18 +1,13 @@
 Describe: beepBoop()
 
-Test: "It should return an array of numbers from 0 to the user's inputted number"
-Code: beepBoop(0);
+Test: "It should return a list of numbers from 0 to the user's inputted number"
+Code: beepBoop("0");
 Expected Output: [0]
 
-Test: "It should replace numbers that contain a "1" with "Beep"
-Code: beepBoop(1);
+Test: "It should replace numbers containing a "1" with "Beep";
+Code: beepBoop("1");
 Expected Output: [0, "Beep"]
 
-Test: "It should replave numbers that contain a "2" with "Boop"
-Code: beepBoop(2);
-Expected Output: [0, "Beep", "Boop"]
-
-Test: "It should replace numbers that contain a 3 with "Won't you be my neighbor?"
-Code: beepBoop(3);
-Expected Output: [0, "Beep", "Boop", "Won't you be my neighbor?"
-]
+Test: "It should replace numbers containing "2" with "Boop";
+Code: beepBoop("2");
+Expected Output: 
